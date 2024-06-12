487f12e28eb9a49e04872d7956e356aafff53824
Chrysis417 <943837838@qq.com>
2024-06-12 23:28:35 +0800

    i18n

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -3,7 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { pinia } from './configs/pinia'
-import { router } from './configs/router'
+import router from './router'
 
 
 createApp(App).use(pinia).use(router).mount('#app')
